dee9c389a6504f3fb4be32e6317fc98b3d123503
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-25 17:21:53 +0900

    install react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
 function App() {
-  return <LoggedOutRouter />;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedOutRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,39 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+export const LoggedOutRouter = () => {
+    const {register, watch, handleSubmit, errors} = useForm();
+    const onSubmit = () => {
+        console.log(watch());
+    };
+    const onInvalid = () => {
+        console.log('cant create account');
+    };
+    console.log(errors);
+    return (
+        <div>
+            <h1>Logged Out</h1>
+            <form onSubmit={handleSubmit(onSubmit, onInvalid)}> 
+                <div>
+                    <input 
+                        ref={register({
+                            required: true,
+                        })}
+                        name="email"
+                        type='email'
+                        placeholder='email'/>
+                </div>
+                <div>
+                    <input 
+                        ref={register({ required: true })}
+                        name="password"
+                        type="password"
+                        required
+                        placeholder="password"
+                        />
+                </div>
+            </form>
+        </div>
+    )
+
+}
