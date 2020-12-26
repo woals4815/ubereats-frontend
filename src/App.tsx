e9970ad50370d68d21f4fa1e67ad89c692048d2e
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-26 19:15:50 +0900

    Login Form CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { isLoggedInVar } from './apollo';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
+
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedOutRouter /> : <LoggedOutRouter />;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,62 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface ILoginForm {
+    email?: string;
+    password?: string;
+}
 
 export const Login = () => {
-    return <span>Login</span>;
-}
+    const {register, getValues, errors, handleSubmit} = useForm<ILoginForm>();
+    const onSubmit = () => {};
+    return (
+        <div className="h-screen flex items-center justify-center bg-gray-800">
+            <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+                <h3 className="text-2xl text-gray-800">Log In</h3>
+                <form 
+                    onSubmit = {handleSubmit(onSubmit)}
+                    className="grid gap-3 mt-5 px-5"
+                    >
+                    <input
+                        ref={register({required: 'Email is required'})}
+                        name="email"
+                        required
+                        type='email'
+                        placeholder="Email"
+                        className="input"
+                    />
+                    {errors.email?.message && (
+                        <span className='font-medium text-red-500'>
+                            {errors.email?.message}
+                        </span>
+                    )}
+                    <input
+                        ref={register({required:'Password is required', minLength: 10})}
+                        required
+                        name='password'
+                        type='password'
+                        placeholder="Password"
+                        className="input"
+                    />
+                    {
+                        errors.password?.message && (
+                            <span className='font-medium text-red-500'>
+                                {errors.password?.message}
+                            </span>
+                        )
+                    }
+                    {
+                        errors.password?.type === 'minLength' && (
+                            <span className='font-medium text-red-500'>
+                                Password must be more than 10 chars.
+                            </span>
+                        )
+                    }
+                    <button className="mt-3 btn">
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
