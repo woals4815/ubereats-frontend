6525bf077f7be4fe18f31d7d91abdfd70f753b1e
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-28 16:57:35 +0900

    Edit ClientRotues to typeof variable

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
 
-const ClientRoutes = () => [
+const ClientRoutes = [
     <Route path="/" exact>
         <Restaurants />
     </Route>,
