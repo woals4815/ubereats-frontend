874231d9391cb2191d69a111c1a603d3935982c3
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-28 15:10:43 +0900

    saving the token and using the token Done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,6 +9,9 @@ import {
 import uberLogo from '../images/eats-logo.svg';
 import { Button } from "../components/button";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { authTokenVar, isLoggedInVar } from "../apollo";
+import { LOCALSTORAGE_TOKEN } from "../constants";
 
 const LOGIN_MUTATION = gql`
 mutation loginMutation($loginInput: LoginInput!) {
@@ -33,8 +36,10 @@ interface ILoginForm {
         const {
           login: { ok, token },
         } = data;
-        if (ok) {
-          console.log(token);
+        if (ok && token) {
+          localStorage.setItem(LOCALSTORAGE_TOKEN, token);
+          authTokenVar(token);
+          isLoggedInVar(true);
         }
       };
     const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
@@ -43,20 +48,23 @@ interface ILoginForm {
     >(LOGIN_MUTATION, {
         onCompleted,
         });
-        const onSubmit = () => {
-        if(!loading){    
-            const { email, password } = getValues();
-            loginMutation({
-            variables: {
-                loginInput: {
-                    email,
-                    password,
-                },
-                },
-            });}
-        };
+    const onSubmit = () => {
+    if(!loading){    
+        const { email, password } = getValues();
+        loginMutation({
+        variables: {
+            loginInput: {
+                email,
+                password,
+            },
+            },
+        });}
+    };
     return (
         <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+            <Helmet>
+                <title>Login | Uber Eats</title>
+            </Helmet>
             <div className=" w-full max-w-screen-sm flex flex-col px-5 items-center">
                 <img src={uberLogo} className="w-52 mb-10" alt="uberLogo"/>
                 <h4 className="w-full font-medium text-left text-3xl mb-5">
@@ -68,7 +76,10 @@ interface ILoginForm {
                 className="grid gap-3 mt-5 w-full mb-5"
                 >
                     <input
-                    ref={register({ required: "Email is required" })}
+                    ref={register({ 
+                        required: "Email is required", 
+                        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    })}
                     name="email"
                     required
                     type="email"
@@ -77,6 +88,9 @@ interface ILoginForm {
                     />
                     {errors.email?.message && (
                     <FormError errorMessage={errors.email?.message} />
+                    )}
+                    {errors.email?.type === "pattern" && (
+                        <FormError errorMessage={'Please enter a valid email'} />
                     )}
                     <input
                     ref={register({ required: "Password is required" })}
@@ -103,7 +117,7 @@ interface ILoginForm {
                 </form>
                 <div>
                     New to Uber?{" "}
-                    <Link to="/creaate-account" className=" text-lime-600 hover:underline">
+                    <Link to="/create-account" className=" text-lime-600 hover:underline">
                         Create an Account
                     </Link>
                 </div>
