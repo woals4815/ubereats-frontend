52e96c5c0cefb55f777b08af982fc77ed867387b
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-28 23:25:49 +0900

    Verify email and Edit Profile with writeFragment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,15 +8,24 @@ import uberLogo from '../images/eats-logo.svg';
 export const Header: React.FC = () => {
     const { data } = useMe();
     return (
-        <header className="py-4">
-            <div className="w-full  px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-                <img src={uberLogo} className="w-24" alt="uberLogo" />
-                <span className="text-xs">
-                    <Link to="/my-profile">
-                        <FontAwesomeIcon icon={faUser} className="text-xl"/>
+        <>
+            {!data?.me.verified && (
+                <div className="bg-red-500 p-3 text-center text-base text-white">
+                    <span>Please verify your email</span>
+                </div>
+            )}
+            <header className="py-4">
+                <div className="w-full  px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
+                    <Link to="/">
+                        <img src={uberLogo} className="w-24" alt="uberLogo" />    
                     </Link>
-                </span>
-            </div>
-        </header>
+                    <span className="text-xs">
+                        <Link to="/edit-profile">
+                            <FontAwesomeIcon icon={faUser} className="text-xl"/>
+                        </Link>
+                    </span>
+                </div>
+            </header>
+        </>
     );
 };
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -3,17 +3,24 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-d
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
+import { ConfirmEmail } from "../pages/user/confirm-email";
+import { EditProfile } from "../pages/user/edit-profile";
 
 const ClientRoutes = [
-    <Route path="/" exact>
+    <Route key={1} path="/" exact>
         <Restaurants />
     </Route>,
+    <Route key={2} path="/confirm" exact>
+        <ConfirmEmail />
+    </Route>,
+    <Route key={3} path="/edit-profile" exact>
+        <EditProfile />
+    </Route>
 ];
 
 
 export const LoggedInRouter = () => {
     const {data, loading, error} = useMe();
-    console.log(error);
     if(loading || !data || error){
         return (
             <div className=" h-screen flex justify-center items-center">
