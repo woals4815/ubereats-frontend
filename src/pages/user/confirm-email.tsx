9342e4b15b9762e529af3e3f3348607d7b1ad433
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2020-12-29 11:55:54 +0900

    Category style

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,5 +1,6 @@
 import { gql, useApolloClient, useMutation } from "@apollo/client"
 import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { useHistory } from "react-router-dom";
 import { useMe } from "../../hooks/useMe";
 import { verifyEmail, verifyEmailVariables } from "../../__generated__/verifyEmail";
@@ -54,6 +55,9 @@ export const ConfirmEmail = () => {
     }, [verifyEmail]);
     return (
         <div className="mt-52 flex flex-col items-center justify-center">
+            <Helmet>
+                <title>Verify Email | Uber Eats</title>
+            </Helmet>
             <h2 className="text-lg mb-1 font-medium">Confirming email</h2>
             <h4 className="text-gray-700 text-sm">
                 Please wait, don't close this page...
--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -1,5 +1,6 @@
 import { gql, useApolloClient, useMutation } from '@apollo/client';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { useForm } from 'react-hook-form';
 import { Button } from '../../components/button';
 import { useMe } from '../../hooks/useMe';
@@ -74,6 +75,11 @@ export const EditProfile = () => {
     };
     return (
         <div className="mt-52 flex  flex-col justify-center items-center">
+            <Helmet>
+                <title>
+                    Edit Profile | Uber Eats
+                </title>
+            </Helmet>
             <h4 className="font-semibold text-2xl mb-3">Edit Profile</h4>
             <form onSubmit={handleSubmit(onSubmit)} className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5">
                 <input ref={register({
