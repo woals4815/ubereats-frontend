784922539bdd393a4fff3c9c733546f3158efb65
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-01-02 21:08:55 +0900

    cashe optimization and restaurant dashboard and Create Dish and Dish options Done

--- src/__generated__/globalTypes.ts
+++ src/__generated__/globalTypes.ts
@@ -24,11 +24,30 @@ export interface CreateAccountInput {
   role: UserRole;
 }
 
+export interface CreateDishInput {
+  name: string;
+  price: number;
+  description: string;
+  options?: DishOptionsInputType[] | null;
+  restaurantId: number;
+}
+
 export interface CreateRestaurantInput {
   name: string;
   coverImg: string;
   address: string;
   categoryName: string;
+}
+
+export interface DishChoiceInputType {
+  name: string;
+  extra?: number | null;
+}
+
+export interface DishOptionsInputType {
+  name: string;
+  choices?: DishChoiceInputType[] | null;
+  extra?: number | null;
 }
 
 export interface EditProfileInput {
@@ -39,6 +58,10 @@ export interface EditProfileInput {
 export interface LoginInput {
   email: string;
   password: string;
+}
+
+export interface MyRestaurantInput {
+  id: number;
 }
 
 export interface RestaurantInput {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
 import { NotFound } from "../pages/404";
@@ -7,7 +7,9 @@ import { Category } from "../pages/client/category";
 import { Restaurant } from "../pages/client/restaurant";
 import { Restaurants } from "../pages/client/restaurants";
 import { Search } from "../pages/client/search";
+import { AddDish } from "../pages/owner/add-dish";
 import { AddRestaurant } from "../pages/owner/add-restaurants";
+import { MyRestaurant } from "../pages/owner/my-restaurant";
 import { MyRestaurants } from "../pages/owner/my-restaurants";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
@@ -50,7 +52,15 @@ const restaurantRoutes = [
     {
         path: "/add-restaurant", 
         component: <AddRestaurant />
-    }
+    },
+    {
+        path: "/restaurants/:id",
+        component: <MyRestaurant />
+    },
+    {
+        path: "/restaurants/:restaurantId/add-dish",
+        component: <AddDish />
+    },
 ];
 
 export const LoggedInRouter = () => {
