3a6da97946eb8d9dd44ab7d3ab207e8e00e94d57
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-01-05 18:24:18 +0900

    Make Order Page for driver and complete dashboard

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,5 +1,29 @@
+import { gql, useMutation, useSubscription } from '@apollo/client';
 import  GoogleMapReact from 'google-map-react';
 import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FULL_ORDER_FRAGMENT } from '../../fragments';
+import { cookedOrders } from '../../__generated__/cookedOrders';
+import { takeOrder, takeOrderVariables } from '../../__generated__/takeOrder';
+
+
+const COOKED_ORDERS_SUBSCRIPTION = gql`
+    subscription cookedOrders {
+        cookedOrders{
+            ...FullOrderParts
+        }
+    }
+    ${FULL_ORDER_FRAGMENT}
+`;
+
+const TAKE_ORDER_MUTATION = gql`
+    mutation takeOrder($input: TakeOrderInput!){
+        takeOrder(input: $input){
+            ok
+            error
+        }
+    }
+`;
 
 interface ICoords{
     lat: number;
@@ -51,14 +75,14 @@ export const Dashboard = () => {
         setMap(map);
         setMaps(maps);
       };
-    const onGetRouteClick = () => {
+    const makeRoute = () => {
         if (map) {
             const directionsService = new google.maps.DirectionsService();
             const directionsRenderer = new google.maps.DirectionsRenderer({
                 polylineOptions: {
                     strokeColor: "#000",
                     strokeOpacity: 0.9,
-                    strokeWeight: 7
+                    strokeWeight: 5
                 }
             });
             directionsRenderer.setMap(map);
@@ -83,6 +107,32 @@ export const Dashboard = () => {
             );
         }
     };
+    const { data: cookedOrdersData } = useSubscription<cookedOrders>(
+        COOKED_ORDERS_SUBSCRIPTION
+    );
+    useEffect(() => {
+        if(cookedOrdersData?.cookedOrders.id){
+            makeRoute();
+        }
+    }, [cookedOrdersData]);
+    const history = useHistory();
+    const onCompleted = (data: takeOrder) => {
+        if(data.takeOrder.ok){
+            history.push(`/orders/${cookedOrdersData?.cookedOrders.id}`);
+        }
+    };
+    const [takeOrderMutation] = useMutation<takeOrder, takeOrderVariables>(
+        TAKE_ORDER_MUTATION, { onCompleted }
+    );
+    const triggerMutation = (orderId: number) => {
+        takeOrderMutation({
+            variables: {
+                input: {
+                    id: orderId,
+                },
+            },
+        });
+    };
     return (
         <div>
             <div
@@ -103,7 +153,31 @@ export const Dashboard = () => {
                     <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
                 </GoogleMapReact>
             </div>
-            <button onClick={onGetRouteClick}>Get route</button>
+            <div className=" max-w-screen-sm mx-auto bg-white relative -top-10 shadow-lg py-8 px-5">
+                {cookedOrdersData?.cookedOrders.restaurant ? (
+                <>
+                    <h1 className="text-center  text-3xl font-medium">
+                    New Coocked Order
+                    </h1>
+                    <h1 className="text-center my-3 text-2xl font-medium">
+                    Pick it up soon @{" "}
+                    {cookedOrdersData?.cookedOrders.restaurant?.name}
+                    </h1>
+                    <button
+                        onClick={() => 
+                            triggerMutation(cookedOrdersData.cookedOrders.id)
+                        }
+                        className="btn w-full  block  text-center mt-5"
+                    >
+                        Accept Challenge &rarr;
+                    </button>
+                </>
+                ) : (
+                <h1 className="text-center  text-3xl font-medium">
+                    No orders yet...
+                </h1>
+                )}
+            </div>
         </div>
     );
 };
